6b287a5fd8ae3f810a5160ff0422b2ee44ef9e22
Ишутин Николай <ishutin55@gmail.com>
2023-01-18 16:59:42 +0000

    Base project

---
+++ impl/src/main/java/ru/skillbox/diplom/group33/social/service/Application.java
@@ -0,0 +1,13 @@
+package ru.skillbox.diplom.group33.social.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
